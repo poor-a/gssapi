2e5e9c7be4ddef7d731fec200144ee39866ee209
Artúr Poór <gombocarti@gmail.com>
2017-12-30 19:42:10 +0100

    Verify initial credentials.

--- clib/hkrb5.c
+++ clib/hkrb5.c
@@ -14,8 +14,10 @@ krb5_error_code _hkrb5_login(krb5_context kcontext, krb5_principal client, char 
     memset(&creds, 0, sizeof(creds));
     krb5_get_init_creds_opt_alloc(kcontext, &gic_options);
     code = krb5_get_init_creds_password(kcontext, &creds, client, password, NULL, NULL, 0, NULL, gic_options);
-    if (!code)
+    if (!code) {
+      code = krb5_verify_init_creds(kcontext, &creds, NULL, NULL, NULL, NULL);
       krb5_free_cred_contents(kcontext, &creds);
+    }
     krb5_get_init_creds_opt_free(kcontext, gic_options);
 
     return code;
